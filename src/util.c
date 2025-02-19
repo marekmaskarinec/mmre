e6b6655e5a59ecf5fbc9550f99afa85dfbfa6921
Marek Maškarinec <marek@mrms.cz>
2025-02-19 14:26:10 +0100

    Fix log off-by-one
    Signed-off-by: Marek Maškarinec <marek@mrms.cz>

--- src/util.c
+++ src/util.c
@@ -35,7 +35,7 @@ _log(enum logLevel level, char *msg, ...) {
 	fprintf(
 	    log_file,
 	    "%04d-%02d-%02dT%02d:%02d:%02d: [%s]: ", tm->tm_year + 1900,
-	    tm->tm_mon, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec,
+	    tm->tm_mon + 1, tm->tm_mday, tm->tm_hour, tm->tm_min, tm->tm_sec,
 	    levelStr[level]
 	);
 
